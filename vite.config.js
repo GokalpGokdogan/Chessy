4522132a4ca50e32a7c41ce336d0cc49f6cb93b1
Gokalp <gokalp.gokdogan@gmail.com>
2024-04-13 22:11:09 +0300

    deploy ready?

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {config} from 'dotenv'
 
+config({path: __dirname + '/backend/.env'})
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: process.env.PORT || 4000,
     proxy: {
-      '/api': 'http://localhost:4000',
+      '/api': 'http://localhost:' + process.env.PORT || 'http://localhost:4000',
     },
   },
 })
