133a038826d09888dea6b9a2f25a8e9a68135a5e
Gokalp <gokalp.gokdogan@gmail.com>
2024-04-19 11:59:20 +0300

    Update Vite configuration and package dependencies

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+// const cors = require('cors')
+// const path = require('path')
 
+// app.use(cors())
 const usersRouter = require('./routes/users');
 const aiRouter = require('./routes/ai');
 // const fishRouter = require('./routes/fish');
@@ -19,6 +22,26 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.log(err));
 
 //middleware
+
+// serve frontend
+// app.use(express.static(path.join(__dirname, './..'), {
+//   setHeaders: (res, path) => {
+//     if (path.endsWith('.jsx')) {
+//       res.type('text/javascript');
+//     }
+//   }
+// }))
+// app.get('*', (req, res, next) => {
+//   res.sendFile(
+//     path.join(__dirname, './../index.html'),
+//     function (err){
+//       if (err) {
+//         console.error('Error in frontend', err);
+//         next(err); // Pass the error to the next middleware
+//       }
+//     }
+//   )
+// })
 
 app.use(express.json());
 app.use((req, res, next) => {
--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import {config} from 'dotenv'
 
+console.log(__dirname+'/backend/.env')
 config({path: __dirname + '/backend/.env'})
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 4000,
+    port: 8080 || process.env.PORT || 4000,
     proxy: {
       '/api': 'http://localhost:' + process.env.PORT || 'http://localhost:4000',
     },
